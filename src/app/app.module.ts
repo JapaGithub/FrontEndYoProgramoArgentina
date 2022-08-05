cdffa0e6e4bf8d2611dc189acbafc0afa4fea2dd
JapaGithub <palfonsojordan@gmail.com>
2022-08-05 11:31:16 -0300

    Header logo/socialMedia/buttonLogin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule,
