944f896c7bdd0c264c278f473289cf71a6ebf7d7
JapaGithub <palfonsojordan@gmail.com>
2022-08-23 14:27:12 -0300

    Estructura basica lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { BannerHeaderComponent } from './components/banner-header/banner-header.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SocialMediaComponent
+    SocialMediaComponent,
+    BannerHeaderComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
